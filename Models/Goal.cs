b7390f6e8b57779f7c112c90b11cd9e19fd2a15d
WarperSan <leumas.ecole@gmail.com>
2025-05-03 17:53:01 -0400

    Goals

---
+++ Models/Goal.cs
@@ -0,0 +1,33 @@
+﻿namespace BingoAPI.Models;
+
+/// <summary>
+/// Representation of a goal
+/// </summary>
+public struct Goal
+{
+    /// <summary>
+    /// Unique ID of this goal
+    /// </summary>
+    /// <remarks>
+    /// If there is another goal with the same value, one of them won't be registered
+    /// </remarks>
+    public string GUID;
+
+    /// <summary>
+    /// Defines if this goal is active or not
+    /// </summary>
+    public bool IsActive;
+    
+    /// <summary>
+    /// Text to show when displaying this goal
+    /// </summary>
+    public string Title;
+
+    /// <summary>
+    /// Converts this goal into a JSON element
+    /// </summary>
+    public string GetJSON()
+    {
+        return $"{{\"name\":\"{Title}\"}}";
+    }
+}
