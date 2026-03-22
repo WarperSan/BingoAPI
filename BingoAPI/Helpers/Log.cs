30147e315ee7a5ba735655cc00f7646bf5e99b35
WarperSan <leumas.ecole@gmail.com>
2026-03-22 18:39:22 -0400

    Use `ILogger` from `Microsoft.Extensions.Logging.Abstractions`

--- BingoAPI/Helpers/Log.cs
+++ BingoAPI/Helpers/Log.cs
@@ -1,45 +1,38 @@
-namespace BingoAPI.Helpers;
+using Microsoft.Extensions.Logging;
 
-/// <summary>
-/// Defines log severity levels
-/// </summary>
-public enum LogLevel : byte
-{
-	Debug,
-	Info,
-	Warning,
-	Error
-}
+namespace BingoAPI.Helpers;
 
 /// <summary>
 /// Class helping for logging stuff
 /// </summary>
 public static class Log
 {
-	private static ILogger? _logger;
-
 	/// <summary>
-	/// Sets the <see cref="ILogger"/> to use
+	/// Instance of <see cref="ILogger"/> to use
 	/// </summary>
-	public static void SetLogger(ILogger logger) => _logger = logger;
+	public static ILogger? Logger
+	{
+		private get;
+		set;
+	}
 
 	/// <summary>
 	/// Logs information for developers that helps to debug the mod
 	/// </summary>
-	internal static void Debug(string? message) => _logger?.Log(message, LogLevel.Debug);
+	internal static void Debug(string? message) => Logger?.LogDebug("{Message}", message);
 
 	/// <summary>
 	/// Logs information for players to know important steps of the mod
 	/// </summary>
-	internal static void Info(string? message) => _logger?.Log(message, LogLevel.Info);
+	internal static void Info(string? message) => Logger?.LogInformation("{Message}", message);
 
 	/// <summary>
 	/// Logs information for players to warn them about an unwanted state
 	/// </summary>
-	internal static void Warning(string? message) => _logger?.Log(message, LogLevel.Warning);
+	internal static void Warning(string? message) => Logger?.LogWarning("{Message}", message);
 
 	/// <summary>
 	/// Logs information for players to notify them of an error
 	/// </summary>
-	internal static void Error(string? message) => _logger?.Log(message, LogLevel.Error);
+	internal static void Error(string? message) => Logger?.LogError("{Message}", message);
 }
