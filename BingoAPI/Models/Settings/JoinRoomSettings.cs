6aeb717a85b7630167dcd96ebd9ff3f74ed496d7
WarperSan <leumas.ecole@gmail.com>
2026-05-29 13:18:45 -0400

    Converted `JoinRoomSettings` to a record

--- BingoAPI/Models/Settings/JoinRoomSettings.cs
+++ BingoAPI/Models/Settings/JoinRoomSettings.cs
@@ -1,26 +1,29 @@
-using BingoAPI.Networking;
+using System.Diagnostics.CodeAnalysis;
+using BingoAPI.Networking.Clients;
 
 namespace BingoAPI.Models.Settings;
 
 /// <summary>
 /// Data used to join a room when calling <see cref="BingoApiClient.JoinRoom"/>
 /// </summary>
-public struct JoinRoomSettings
+[SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public record JoinRoomSettings
 {
 	/// <summary>
 	/// Code of the room
 	/// </summary>
-	public string Code { get; set; }
+	public string Code { get; set; } = string.Empty;
 
 	/// <summary>
 	/// Password of the room
 	/// </summary>
-	public string Password { get; set; }
+	public string Password { get; set; } = string.Empty;
 
 	/// <summary>
 	/// Name of the player to connect as
 	/// </summary>
-	public string Nickname { get; set; }
+	public string Nickname { get; set; } = string.Empty;
 
 	/// <summary>
 	/// Should the user be connected as a spectator or not
