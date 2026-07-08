9e79870b4c0954e97066983c9df1fd2770565fd4
WarperSan <leumas.ecole@gmail.com>
2026-07-08 08:43:29 -0400

    Changed `ConditionRegistry` to now register types with actions

--- BingoAPI/Conditions/ConditionRegistry.cs
+++ BingoAPI/Conditions/ConditionRegistry.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
+using BingoAPI.Helpers;
 using JetBrains.Annotations;
 
 namespace BingoAPI.Conditions;
@@ -9,18 +11,7 @@ namespace BingoAPI.Conditions;
 [PublicAPI]
 public static class ConditionRegistry
 {
-	private static readonly Dictionary<string, Func<ConditionData, ICondition>> Factories = new(StringComparer.OrdinalIgnoreCase);
-
-	/// <summary>
-	/// Tries to add the given factory under the given action key
-	/// </summary>
-	public static void TryAdd(string action, Func<ConditionData, ICondition> factory)
-	{
-		if (Factories.ContainsKey(action))
-			return;
-
-		Factories.Add(action, factory);
-	}
+	private static readonly Dictionary<string, Type> TypePerAction = new(StringComparer.OrdinalIgnoreCase);
 
 	/// <summary>
 	/// Adds every <see cref="ICondition"/> defined using <see cref="ConditionAttribute"/>
@@ -45,7 +36,10 @@ public static class ConditionRegistry
 				if (type == null)
 					continue;
 
-				if (!type.IsAssignableFrom(typeof(ICondition)))
+				if (type.IsAbstract || type.IsInterface)
+					continue;
+
+				if (!typeof(ICondition).IsAssignableFrom(type))
 					continue;
 
 				var attribute = type.GetCustomAttribute<ConditionAttribute>();
@@ -53,20 +47,14 @@ public static class ConditionRegistry
 				if (attribute == null)
 					continue;
 
-				// TODO: Change register
-				//TryAdd(attribute.Action, data => );
+				Log.Debug($"Registering '{type}' under '{attribute.Action}'.");
+				TypePerAction[attribute.Action] = type;
 			}
 		}
 	}
 
 	/// <summary>
-	/// Creates a <see cref="ICondition"/> registered under the given action
+	/// Attempts to find the type associated with the given action
 	/// </summary>
-	internal static ICondition Create(string action, ConditionData data)
-	{
-		if (!Factories.TryGetValue(action, out var factory))
-			throw new InvalidOperationException($"No condition registered under the action '{action}'.");
-
-		return factory.Invoke(data);
-	}
+	public static bool TryGetType(string action, [NotNullWhen(true)] out Type? type) => TypePerAction.TryGetValue(action, out type);
 }
