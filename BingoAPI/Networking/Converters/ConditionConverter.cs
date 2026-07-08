0579721c138106b2e44ee4b9f67ef89c2f8d3420
WarperSan <leumas.ecole@gmail.com>
2026-07-08 08:44:43 -0400

    Changed `ConditionConverter` to handle proper types

--- BingoAPI/Networking/Converters/ConditionConverter.cs
+++ BingoAPI/Networking/Converters/ConditionConverter.cs
@@ -1,4 +1,5 @@
 using BingoAPI.Conditions;
+using BingoAPI.Helpers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -7,28 +8,30 @@ namespace BingoAPI.Networking.Converters;
 /// <summary>
 /// Converts a <see cref="string"/> to a <see cref="ICondition"/>
 /// </summary>
-internal class ConditionConverter : JsonConverter<ICondition>
+internal class ConditionConverter : JsonConverter
 {
 	private const string ACTION_KEY = "action";
+	private const string PARAMS_KEY = "params";
 
 	/// <inheritdoc />
 	public override bool CanWrite => false;
 
 	/// <inheritdoc />
-	public override void WriteJson(JsonWriter writer, ICondition? value, JsonSerializer serializer)
+	public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
 	{
-		throw new NotImplementedException();
+		throw new InvalidOperationException();
 	}
 
 	/// <inheritdoc />
-	public override ICondition ReadJson(
+	public override object ReadJson(
 		JsonReader reader,
 		Type objectType,
-		ICondition? existingValue,
-		bool hasExistingValue,
+		object? existingValue,
 		JsonSerializer serializer
 	)
 	{
+		Log.Info("B: " + objectType.Name);
+
 		var obj = JObject.Load(reader);
 
 		var action = obj.Value<string>(ACTION_KEY);
@@ -36,8 +39,18 @@ internal class ConditionConverter : JsonConverter<ICondition>
 		if (action == null)
 			throw new JsonException($"Expected '{ACTION_KEY}' property: {obj}");
 
-		var data = new ConditionData(obj);
+		if (!ConditionRegistry.TryGetType(action, out var type))
+			throw new InvalidOperationException($"No condition has been registered under '{action}'.");
 
-		return ConditionRegistry.Create(action, data);
+		if (!obj.TryGetValue(PARAMS_KEY, out var paramsToken))
+			throw new JsonException($"Expected '{PARAMS_KEY}' property: {obj}");
+
+		if (paramsToken.ToObject(type) is not ICondition condition)
+			throw new JsonException($"Failed to parse '{type}': {paramsToken}");
+
+		return condition;
 	}
+
+	/// <inheritdoc />
+	public override bool CanConvert(Type objectType) => objectType == typeof(ICondition);
 }
