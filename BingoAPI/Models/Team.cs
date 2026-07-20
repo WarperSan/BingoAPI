6dc63799675d9f982043a719a822fe497ca6a63e
WarperSan <leumas.ecole@gmail.com>
2026-07-20 13:30:59 -0400

    Added `[EnumMember]` for Team values

--- BingoAPI/Models/Team.cs
+++ BingoAPI/Models/Team.cs
@@ -1,3 +1,4 @@
+using System.Runtime.Serialization;
 using BingoAPI.Networking.Converters;
 using Newtonsoft.Json;
 
@@ -11,16 +12,37 @@ namespace BingoAPI.Models;
 public enum Team : ushort
 {
 #pragma warning disable CS1591
+	[EnumMember(Value = "blank")]
 	None = 0,
+
+	[EnumMember(Value = "pink")]
 	Pink = 1 << 0,
+
+	[EnumMember(Value = "red")]
 	Red = 1 << 1,
+
+	[EnumMember(Value = "orange")]
 	Orange = 1 << 2,
+
+	[EnumMember(Value = "brown")]
 	Brown = 1 << 3,
+
+	[EnumMember(Value = "yellow")]
 	Yellow = 1 << 4,
+
+	[EnumMember(Value = "green")]
 	Green = 1 << 5,
+
+	[EnumMember(Value = "teal")]
 	Teal = 1 << 6,
+
+	[EnumMember(Value = "blue")]
 	Blue = 1 << 7,
+
+	[EnumMember(Value = "navy")]
 	Navy = 1 << 8,
+
+	[EnumMember(Value = "purple")]
 	Purple = 1 << 9,
 #pragma warning restore CS1591
 }
