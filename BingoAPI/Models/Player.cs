27efa87cb6655ea12a42ce86357d4517da9cc9d9
WarperSan <leumas.ecole@gmail.com>
2026-08-15 09:13:49 -0400

    Player + Team

---
+++ BingoAPI/Models/Player.cs
@@ -0,0 +1,22 @@
+namespace BingoAPI.Models;
+
+/// <summary>
+/// Represents a player in the room
+/// </summary>
+public record Player
+{
+	/// <summary>
+	/// Unique identifier of this player
+	/// </summary>
+	public required string UUID { get; init; }
+
+	/// <summary>
+	/// Display name of this player
+	/// </summary>
+	public required string Name { get; init; }
+
+	/// <summary>
+	/// Team this player belongs to
+	/// </summary>
+	public required Team Team { get; init; }
+}
---
+++ BingoAPI/Models/Team.cs
@@ -0,0 +1,22 @@
+namespace BingoAPI.Models;
+
+/// <summary>
+/// Teams available for a bingo match
+/// </summary>
+[Flags]
+public enum Team : ushort
+{
+#pragma warning disable CS1591
+	None = 0,
+	Pink = 1 << 0,
+	Red = 1 << 1,
+	Orange = 1 << 2,
+	Brown = 1 << 3,
+	Yellow = 1 << 4,
+	Green = 1 << 5,
+	Teal = 1 << 6,
+	Blue = 1 << 7,
+	Navy = 1 << 8,
+	Purple = 1 << 9,
+#pragma warning restore CS1591
+}
