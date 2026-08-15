25a8d19544fcca4bfe19093eff1daeb60c847571
WarperSan <leumas.ecole@gmail.com>
2026-08-15 18:46:26 -0400

    Settings

---
+++ BingoAPI/Configuration/Settings/CreateRoomSettings.cs
@@ -0,0 +1,43 @@
+using JetBrains.Annotations;
+
+namespace BingoAPI.Configuration.Settings;
+
+/// <summary>
+/// Settings used when creating a room
+/// </summary>
+[PublicAPI]
+public record CreateRoomSettings
+{
+	/// <summary>
+	/// Name of the room
+	/// </summary>
+	public required string Name { get; init; }
+
+	/// <summary>
+	/// Password of the room
+	/// </summary>
+	public required string Password { get; init; }
+
+	/// <summary>
+	/// Name of the player to connect as
+	/// </summary>
+	public required string Nickname { get; init; }
+
+	/// <summary>
+	/// Should the room be randomized or not
+	/// </summary>
+	public bool IsRandomized { get; set; }
+
+	/// <summary>
+	/// Should the room be in lockout or not
+	/// </summary>
+	public bool IsLockout { get; set; }
+
+	/// <summary>
+	/// Seed to use for the randomization
+	/// </summary>
+	/// <remarks>
+	/// Leave it empty if you want the seed to be automatically generated
+	/// </remarks>
+	public string Seed { get; set; } = string.Empty;
+}
---
+++ BingoAPI/Configuration/Settings/JoinRoomSettings.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+
+namespace BingoAPI.Configuration.Settings;
+
+/// <summary>
+/// Settings used when joining a room
+/// </summary>
+[PublicAPI]
+public record JoinRoomSettings
+{
+	/// <summary>
+	/// Code of the room
+	/// </summary>
+	public required string Code { get; init; }
+
+	/// <summary>
+	/// Password of the room
+	/// </summary>
+	public required string Password { get; init; }
+
+	/// <summary>
+	/// Name of the player to connect as
+	/// </summary>
+	public required string Nickname { get; init; }
+}
