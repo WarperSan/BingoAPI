a9efb8cfd3e9d82134bfad900a010579ae55c070
WarperSan <leumas.ecole@gmail.com>
2026-08-17 15:44:02 -0400

    Added `BingoAPI.Network.Response` for better handling of `HttpResponseMessage`

---
+++ BingoAPI/Network/Response.cs
@@ -0,0 +1,84 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace BingoAPI.Network;
+
+/// <summary>
+/// Wrapper to handle <see cref="HttpResponseMessage"/> better
+/// </summary>
+public sealed class Response<T>
+	where T : class
+{
+	private readonly HttpResponseMessage _response;
+
+	/// <summary>
+	/// Determines if the response has been a success
+	/// </summary>
+	public readonly bool IsSuccess;
+
+	/// <summary>
+	/// JSON content of the response if success
+	/// </summary>
+	public readonly T? Data;
+
+	private Response(HttpResponseMessage response, T data)
+	{
+		_response = response;
+		IsSuccess = true;
+		Data = data;
+	}
+
+	/// <summary>
+	/// Creates a new instance of <see cref="Response{T}"/> from the given response
+	/// </summary>
+	public static Response<T> CreateResponse(
+		HttpResponseMessage response,
+		string content,
+		JsonSerializer serializer
+	)
+	{
+		if (response.IsSuccessStatusCode)
+			return HandleSuccess(response, content, serializer);
+
+		var jToken = JToken.Parse(content);
+
+		throw new NotSupportedException($"Received a payload that was not supported:\n{jToken}");
+	}
+
+	private static TPayload ParseJson<TPayload>(string content, JsonSerializer serializer)
+	{
+		TPayload? json;
+
+		try
+		{
+			using var reader = new JsonTextReader(new StringReader(content));
+
+			json = serializer.Deserialize<TPayload>(reader);
+		}
+		catch (JsonException e)
+		{
+			throw new InvalidOperationException(
+				$"Failed to deserialize the response: \n\n{content}",
+				e
+			);
+		}
+
+		if (json == null)
+			throw new NullReferenceException(
+				$"Failed to parse the response's payload as '{nameof(T)}'"
+			);
+
+		return json;
+	}
+
+	private static Response<T> HandleSuccess(
+		HttpResponseMessage response,
+		string content,
+		JsonSerializer serializer
+	)
+	{
+		var data = ParseJson<T>(content, serializer);
+
+		return new Response<T>(response, data);
+	}
+}
